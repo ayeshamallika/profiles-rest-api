0a129ea40eae23e492eb59abd282522ea780811d
Ayesha_Mallika <ayesh.cta@gmail.com>
2022-01-05 21:28:53 +0530

    update view apis

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profile_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-veiwset')
 
 urlpatterns = [
     path('hello-view/',views.HellopApiView.as_view()),
+    path('', include(router.urls))
+
 ]
